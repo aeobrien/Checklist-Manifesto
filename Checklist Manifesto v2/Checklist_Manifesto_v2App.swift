a4d9f82effa24ea89a84965e4e34b364fcd3fa31
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-06-07 13:41:27 +0100

    Initial Commit

---
+++ Checklist Manifesto v2/Checklist_Manifesto_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Checklist_Manifesto_v2App.swift
+//  Checklist Manifesto v2
+//
+//  Created by Aidan O'Brien on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Checklist_Manifesto_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
