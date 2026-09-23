93650ad11ba16fc5ec798d74ce42073949136817
Commit Guardian <guardian@localhost>
2026-09-23 02:03:27 +0100

    Commit Guardian snapshot aidans-mac-mini 2026-09-23 ChecklistManifesto-1134abdaba42

---
+++ snapshot/Checklist Manifesto v2/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Checklist Manifesto v2
+//
+//  Created by Aidan O'Brien on 07/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TagsView()
+            .preferredColorScheme(.light)
+    }
+}
+
+#Preview {
+    ContentView()
+}
